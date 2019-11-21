c072162532f9890e0222a1647993ea85e713a059
Abou <46964181+abd-koumare@users.noreply.github.com>
2019-11-21 21:58:54 +0000

    Update quick_sort.cpp

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -5,16 +5,16 @@ typedef unsigned int uint;
 
 template <typename T>
 void _swap(T &a, T &b){
-	 T temp = a;
-          a = b;
-          b = temp;
+    T temp = a;
+    a = b;
+    b = temp;
 }
 
 
 template <typename T>
 uint _partition(std::vector<T> &arr, int left, int right){
 	 
-	 int j = left - 1;
+     int j = left - 1;
      uint  pivot = right;
      for(int i = left; i < right; i++){
 		 if(arr[i] <= arr[pivot]){
@@ -30,28 +30,31 @@ uint _partition(std::vector<T> &arr, int left, int right){
 template <typename T>
 void quick_sort(std::vector<T> &arr, int left, int right){
 	 if(left < right){
-		int q = _partition(arr, left, right);// From begin to end.
-        quick_sort(arr, left, q - 1);  // From begin to pivot.
-        quick_sort(arr, q + 1, right); // From pivot to end.
-     }
+	     int q = _partition(arr, left, right);// From begin to end.
+             quick_sort(arr, left, q - 1);  // From begin to pivot.
+             quick_sort(arr, q + 1, right); // From pivot to end.
+          }
 }
 
 
 template <typename T>
 void ccontent_of(std::vector<T> arr){
-	 for(auto x : arr)
+    for(auto x : arr)
          std::cout << x << " ";
      std::cout << "\n";
 }
 
 int main()
 {
-	std::vector<int> arr{0,4,6,3,2,10,9};
-	std::cout << "Begin\n";
+    std::vector<int> arr{0,4,6,3,2,10,9};
+	
+    std::cout << "Begin\n";
     ccontent_of(arr);
+	
     std::cout << "Quicksort\n";
     quick_sort(arr,0,6); 
     ccontent_of(arr);
-	std::cout << "End!\n";
-	return 0;
+	
+    std::cout << "End!\n";
+    return 0;
 }
