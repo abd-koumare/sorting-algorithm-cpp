721ecd71bce2b9a89570cff28d0811c559ff357b
Abou <46964181+abd-koumare@users.noreply.github.com>
2019-11-21 22:02:32 +0000

    Update selection_sort.cpp

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -5,19 +5,19 @@ typedef unsigned int uint;
 template <typename T>
 void _swap(T &a, T &b){
    T temp = a;
-        a = b;
-        b = temp;
+   a = b;
+   b = temp;
 }
 
 template<typename T>
-void selection_sort(std::vector<T> &arr, uint left, uint right){
+void selection_sort(std::vector<T> &arr, uint left, uint right) {
 	 
-     for(uint i = left; i <= right; i++){
+     for(uint i = left; i <= right; i++) {
          T minT = arr[i];
          uint minTIndex = i;
-        for(uint j = i + 1; j <= right; j++){
-			if(arr[j] < minT)
-			   minTIndex = j;
+        for(uint j = i + 1; j <= right; j++) {
+	   		if(arr[j] < minT)
+		    	minTIndex = j;
 		}
 		_swap(arr[i],arr[minTIndex]);
      }
@@ -33,8 +33,11 @@ void ccontent_of(std::vector<T> arr){
 int main()
 {
 	std::vector<int> arr{9,8,7,6,5,4,3,2,1,0};
+	
     std::cout << "Begin\n";
 	ccontent_of(arr);
+	
+	
     std::cout << "Selection sort: \n";
     selection_sort(arr,0,9);
     ccontent_of(arr);
